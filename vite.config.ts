4bc93dc4c39793870aedadfc3d0ae2b5e2346f85
Dmitry Petsevich <dmitry.petsevich@gmail.com>
2025-10-21 15:05:04 +0300

    Настройка проекта. Установил зависимости, настроил импорты.

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    },
+  },
+});
